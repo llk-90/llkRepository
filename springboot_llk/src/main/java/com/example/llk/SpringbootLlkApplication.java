19041130d525dce7e6473e97d45059d8f03a8a82
llk-90 <33407076+llk-90@users.noreply.github.com>
2018-06-15 17:19:36 +0800

    add

--- springboot_llk/src/main/java/com/example/llk/SpringbootLlkApplication.java
+++ springboot_llk/src/main/java/com/example/llk/SpringbootLlkApplication.java
@@ -1,12 +1,17 @@
 package com.example.llk;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2 
+@EnableSwagger2
+@ComponentScan("com.example.llk")
+@MapperScan("com.example.llk.dao")
 public class SpringbootLlkApplication {
 
 	public static void main(String[] args) {
---
+++ springboot_llk/src/main/java/com/example/llk/controller/userController/UserController.java
@@ -0,0 +1,25 @@
+package com.example.llk.controller.userController;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/first")
+public class UserController {
+	@ApiResponses({
+     	@ApiResponse(code = 404, message = "服务器内部异常"),
+		})
+	@ApiOperation(value="第一个")
+	@ApiImplicitParam(name = "name",value="需要输入的内容",required =false,paramType ="path",dataType= "String")
+	@RequestMapping("/hello/{name}")
+	public String first(@PathVariable("name") String name) {
+		return name;
+	}
+}
