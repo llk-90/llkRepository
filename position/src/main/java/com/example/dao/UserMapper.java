c1a47eaf1ad62ec83fa699a63572481b3386024d
llk-90 <33407076+llk-90@users.noreply.github.com>
2018-07-13 17:08:17 +0800

    u
    u

--- position/src/main/java/com/example/dao/UserMapper.java
+++ position/src/main/java/com/example/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.dao;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.mapstruct.Mapper;
 
@@ -8,12 +9,20 @@ import com.example.entity.UserPositionInfo;
 
 @Mapper
 public interface UserMapper {
-	
-	//添加定位坐标
-	public void addUserPosition(UserPositionInfo up);
-	
-	//查找历史坐标
-	public ArrayList<String> getPosition(String openId);
-	
-	
+
+	// 添加定位坐标
+	public void addUserInfo(UserPositionInfo up);
+
+	// 查找历史坐标
+	public List<UserPositionInfo> getPosition(String openId);
+
+	// 编辑
+	public void updateInfo(UserPositionInfo up);
+
+	// 查询拜访信息
+	public List<UserPositionInfo> fingBaifangYuyue(int pageCount);
+
+	// 查询客户具体信息
+	public UserPositionInfo findBaifangYuyueDetail(int id);
+
 }
